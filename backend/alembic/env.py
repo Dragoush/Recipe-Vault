2fe7d99e6c1a27aa50ea2f26023058d5bb2531ce
dragoshategangheorghe <dragoshategan3@gmail.com>
2026-06-03 17:55:50 +0300

    Changed alembic db_url init

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,10 +8,7 @@ from app.db.base import Base
 from app.db import models  # noqa: F401
 
 config = context.config
-configured_url = config.get_main_option("sqlalchemy.url")
-
-if not configured_url:
-    config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
